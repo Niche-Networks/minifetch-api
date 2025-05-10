909d7b6e6a98cf0af486f029198d9da5a60121c3
Lauren Garcia <lauren.garcia@gmail.com>
2025-05-09 18:30:24 -0700

    add .env NETWORK & EXPLORER_BASE_URL, improve console output

--- index.ts
+++ index.ts
@@ -5,24 +5,28 @@ import { decodeXPaymentResponse, wrapFetchWithPayment } from "x402-fetch";
 
 // Determine which env file to use
 const args = process.argv.slice(2);
-const envArg = args.find(arg => arg.startsWith('--env='));
-const env = envArg ? envArg.split('=')[1] : 'default';
-console.log('env: ' + env);
+const envArg = args.find(arg => arg.startsWith("--env="));
+const env = envArg ? envArg.split("=")[1] : "default";
+console.log("env: " + env);
 
-let envPath = '.env';
-if (env === 'base-testnet') {
-  envPath = '.env-base-testnet';
-} else if (env === 'local') {
-  envPath = '.env-local';
+let envPath = ".env";
+if (env === "base-testnet") {
+  envPath = ".env-base-testnet";
+} else if (env === "local") {
+  envPath = ".env-local";
 }
-console.log('using env file: ' + envPath);
+console.log("using env file: " + envPath);
 
 // Set dotenv config to use correct .env file
 config({ path: envPath });
-console.log('using network: ' + process.env.NETWORK);
+console.log("using network: " + process.env.NETWORK);
 
-// Set x402 constants from env file
+// Set x402 constants from env
 const privateKey = `0x${process.env.PRIVATE_KEY}` as Hex;
+const explorerBaseUrl = process.env.EXPLORER_BASE_URL as string;
+const account = privateKeyToAccount(privateKey);
+
+// Set url to query from env
 const baseURL = process.env.RESOURCE_SERVER_URL as string; // e.g. https://example.com
 const endpointPath = process.env.ENDPOINT_PATH as string; // e.g. /weather
 const url = `${baseURL}${endpointPath}`; // e.g. https://example.com/weather
@@ -31,25 +35,25 @@ if (!baseURL || !privateKey || !endpointPath) {
   console.error("Missing required environment variables");
   process.exit(1);
 }
+console.log("querying url via fetchWithPayment: " + url);
 
-const account = privateKeyToAccount(privateKey);
+// Fetch url endpoint with payment
 const fetchWithPayment = wrapFetchWithPayment(fetch, account);
-
-console.log('querying url via fetchWithPayment: ' + url);
 fetchWithPayment(url, {
   method: "GET",
 })
   .then(async response => {
     const body = await response.json();
-    console.log('response body:');
+    console.log("response body:");
     console.log(body);
 
     const paymentResponse = decodeXPaymentResponse(response.headers.get("x-payment-response")!);
-    console.log('payment response:');
+    console.log("payment response:");
     console.log(paymentResponse);
+    console.log(`View transaction: ${explorerBaseUrl}/tx/${paymentResponse.transaction}`);
   })
   .catch(error => {
-    console.log('ERROR!')
+    console.log("ERROR!");
     console.dir(error);
     console.error(error.response?.data?.error);
   });
